6d4f44d953c7e4d1782dd7eaa312cb869c6f568e
JessYu-1011 <learningen13@gmail.com>
2022-01-24 10:07:17 +0800

    Add test api

--- backend/app/apis/__init__.py
+++ backend/app/apis/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from flask_restx import Api, Namespace
+from flask_restx import Api
+from .test_api import test as ns1
 
 blueprint = Blueprint('api', __name__)
 
@@ -11,4 +12,4 @@ api = Api(
 )
 
 # Api factory
-api.add_namespace()
+api.add_namespace(ns1, path="/test")
