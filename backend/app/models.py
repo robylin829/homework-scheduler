c47b6c68f4623f05a9685e7d0d6275f48bd62049
JessYu-1011 <learningen13@gmail.com>
2022-01-24 12:46:16 +0800

    Done database schema

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,3 @@
-from enum import unique
-
-from sqlalchemy import null
 from app import db
 
 # Schedule - course - teacher
@@ -10,15 +7,17 @@ class Schedules(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # When is the course
     day = db.Column(db.String(10), nullable=False)
-    courses = db.relationship("Courses", backref="schedules")
+    courses = db.relationship('Courses', backref='schedules')
 
 # Teachers
 class Teachers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
 
 # Courses
 class Courses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_name = db.Column(db.String(50), unique=True, nullable=False)
+    schedule_id = db.Column(db.Integer, db.ForeignKey('schedules.id'))
+    teachers = db.relationship('Teachers', backref='courses')
