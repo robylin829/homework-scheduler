1d61220c8f6704f456121546fdcee376ce993af0
JessYu-1011 <learningen13@gmail.com>
2022-01-24 16:04:05 +0800

    Add put delete into course.py api

--- backend/app/apis/course.py
+++ backend/app/apis/course.py
@@ -9,23 +9,22 @@ api = Namespace(
 )
 
 get_course_data = api.model('Model', {
+    'id': fields.Integer(),
     'course_name': fields.String(),
 })
 
-@api.route('/get')
-class GetCourse(Resource):
+create_course_data = api.model('Model', {
+    'course_name': fields.String()
+})
+
+@api.route('/')
+class CoursesAPI(Resource):
     @api.marshal_with(get_course_data)
     def get(self):
         courses = Courses.query.order_by(Courses.id).all()
         courses_output = courses_shcema.dump(courses)
         return courses_output
 
-create_course_data = api.model('Model', {
-    'course_name': fields.String()
-})
-
-@api.route('/create')
-class CreateCourse(Resource):
     @api.expect(create_course_data)
     def post(self):
         data = api.payload
@@ -34,3 +33,27 @@ class CreateCourse(Resource):
         db.session.add(course)
         db.session.commit()
         return data
+
+@api.route('/<int:id>')
+class CourseAPI(Resource):
+    def getCourses(self, id):
+        return Courses.query.filter_by(id=id).first()
+
+    def get(self, id):
+        course = self.getCourses(id)
+        course_ouput = course_schema.dump(course)
+        return course_ouput
+
+    def delete(self, id):
+        course = self.getCourses(id)
+        db.session.delete(course)
+        db.session.commit()
+        return {"message": "Delete Success"}
+
+    @api.expect(get_course_data)
+    def put(self, id):
+        data = api.payload
+        course = self.getCourses(id)
+        course.course_name = data['course_name']
+        db.session.add(course)
+        db.session.commit()
