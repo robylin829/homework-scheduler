40a911c2c356949e2a02d86301a84895db8145bf
Brainlesstorming <93087761+Brainlesstorming@users.noreply.github.com>
2022-04-08 23:04:24 +0800

    Make the variable name meaningful.
    Make the variable name meaningful.

--- data-parser/classtable_parser.py
+++ data-parser/classtable_parser.py
@@ -8,37 +8,37 @@ f.close()
 for all_class in range(len(os.listdir())-1):
   class_num = os.listdir()[all_class][:os.listdir()[all_class].find('.')]
   f=open(os.listdir()[all_class],'r',encoding='utf-8')
-  dumb=f.read()
-  dumb=dumb.replace(' ','').replace('\n','')
+  original_html=f.read()
+  original_html=original_html.replace(' ','').replace('\n','')
   f.close()
-  if len(dumb) > 1000 and os.listdir()[all_class] not in ['all_classtable.txt',os.path.basename(__file__)]:
-    day=[[],[],[],[],[],[],[]]
-    dumb = dumb.split('</tr>')
-    dumb = dumb[2:]
-    dumb.pop(4)
-    dumb.pop(-1)
-    for i in range(len(dumb)):
-      dumb[i]=dumb[i][dumb[i].find('節<BR></td>'):]
-    for i in range(len(dumb)):
-      if dumb[i].find('</td>') != -1:
-        dumb[i]=dumb[i].split('</td>')
-        dumb[i].pop(-1)
+  if len(original_html) > 1000 and os.listdir()[all_class] not in ['all_classtable.txt',os.path.basename(__file__)]:
+    class_daily=[[],[],[],[],[],[],[]]
+    original_html = original_html.split('</tr>')
+    original_html = original_html[2:]
+    original_html.pop(4)
+    original_html.pop(-1)
+    for i in range(len(original_html)):
+      original_html[i]=original_html[i][original_html[i].find('節<BR></td>'):]
+    for i in range(len(original_html)):
+      if original_html[i].find('</td>') != -1:
+        original_html[i]=original_html[i].split('</td>')
+        original_html[i].pop(-1)
         
-        for j in range(len(dumb[i])):
-          dumb[i][j]=dumb[i][j][(len(dumb[i][j])-dumb[i][j].rfind('>')):]
-          if dumb[i][j].find('</a>') != -1:
-            dumb[i][j]=dumb[i][j].split('</a>')
-            dumb[i][j].pop(-1)
-            for k in range(len(dumb[i][j])):
-              dumb[i][j][k]=dumb[i][j][k][(dumb[i][j][k].rfind('>')+1):]
+        for j in range(len(original_html[i])):
+          original_html[i][j]=original_html[i][j][(len(original_html[i][j])-original_html[i][j].rfind('>')):]
+          if original_html[i][j].find('</a>') != -1:
+            original_html[i][j]=original_html[i][j].split('</a>')
+            original_html[i][j].pop(-1)
+            for k in range(len(original_html[i][j])):
+              original_html[i][j][k]=original_html[i][j][k][(original_html[i][j][k].rfind('>')+1):]
           else:
-            dumb[i][j]=''
-          day[j].append(dumb[i][j])
-    classtable[class_num]=day
+            original_html[i][j]=''
+          class_daily[j].append(original_html[i][j])
+    classtable[class_num]=class_daily
     f=open('all_classtable.txt','r',encoding='utf-8')
-    if str(day) not in f.read():
+    if str(class_daily) not in f.read():
         f=open('all_classtable.txt','a',encoding='utf-8')
-        f.write(os.listdir()[all_class][:os.listdir()[all_class].find('.')]+str(day)+'\n')
+        f.write(os.listdir()[all_class][:os.listdir()[all_class].find('.')]+str(class_daily)+'\n')
     f.close()
 print('done!')
 os.system('pause')
